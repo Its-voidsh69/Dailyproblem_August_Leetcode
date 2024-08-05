73fc6ab6ca1dcf9515ec6adb1502d6a3605bddec
Sumit <sum.itvoid00@gmail.com>
2024-08-05 17:32:54 +0530

    5Aug

--- 5-Aug/5_Aug.java
+++ 5-Aug/5_Aug.java
@@ -7,7 +7,39 @@
 
 //     Note that the strings are considered in the order in which they appear in the array.</p>
 
+
+//Brute Force Solution
+
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        int n = arr.length;
+        String res[] = new String[n];
+        int resIndex = 0;
+
+        for(int i = 0; i<n; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for(int i  = 0;  i<n; i++){
+            if(map.get(arr[i]) == 1){
+                res[resIndex++] = arr[i];
+            }
+        }
+
+        if(resIndex>=k){
+            return res[k-1];
+        }
+
+        return "";
+    }
+}
+
+// Space and Time Complexity
+// Brute Force Solution: Time Complexity - O(n), Space Complexity - O(n)
+
 //Optimal Solution
+
 class Solution {
     public String kthDistinct(String[] arr, int k) {
         HashMap<String, Integer> map = new HashMap<>();
